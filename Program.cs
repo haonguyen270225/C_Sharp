3ea19a2bc3972ab3f76f19a6f2771890eec406e0
haonguyennhat <162509494+haonguyen270225@users.noreply.github.com>
2024-11-18 15:22:10 +0700

    Sinh_So_ngau_Nhien_C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Random rand = new Random();
+int so = 0;
+
+Console.WriteLine("Truong hop  1 : ");
+for( int i = 0; i < 10; i++)
+{
+    so = rand.Next();
+    Console.WriteLine("So ngau nhien la : {0} ", so);
+}
+
+Console.WriteLine("Truong hop  2 : ");
+for ( int i = 0; i < 10; i++)
+{
+    so = rand.Next(5);
+    Console.WriteLine("So ngau nhien la : {0} ", so);
+}
+
+Console.WriteLine("Truong hop  3 : ");
+for (int i = 0; i < 10; i++)
+{
+    so = rand.Next(-10 , 5);
+    Console.WriteLine("So ngau nhien la : {0}", so);
+}
