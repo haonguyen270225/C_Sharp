4f0ac95a4bfc782dfce425c3ae93d4ccae93ade0
haonguyennhat <162509494+haonguyen270225@users.noreply.github.com>
2024-11-18 15:23:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,20 +6,20 @@ int so = 0;
 Console.WriteLine("Truong hop  1 : ");
 for( int i = 0; i < 10; i++)
 {
-    so = rand.Next();
+    so = rand.Next(); // Ham cho ra gia tri nguyen lon hơn bằng 0;
     Console.WriteLine("So ngau nhien la : {0} ", so);
 }
 
 Console.WriteLine("Truong hop  2 : ");
 for ( int i = 0; i < 10; i++)
 {
-    so = rand.Next(5);
+    so = rand.Next(5); // ham cho ra gia tri <= 5-1;
     Console.WriteLine("So ngau nhien la : {0} ", so);
 }
 
 Console.WriteLine("Truong hop  3 : ");
 for (int i = 0; i < 10; i++)
 {
-    so = rand.Next(-10 , 5);
+    so = rand.Next(-10 , 5); // ham cho ra gia tri từ -10->5-1;
     Console.WriteLine("So ngau nhien la : {0}", so);
 }
