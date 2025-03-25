05fea29bf290fcd626e78a517e2b01591d35b765
haonguyennhat <162509494+haonguyen270225@users.noreply.github.com>
2025-03-25 22:06:51 +0700

    Winform_ListBox

---
+++ Demo2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_Hoa_Winform
+{
+    public partial class Demo2 : Form 
+    {
+        public DialogResult Yes { get; private set; }
+
+        public Demo2()
+        {
+            InitializeComponent();
+        }
+
+        #region BienToanCuc
+        int solong = 0;
+        #endregion
+        private void Demo2_Load(object sender, EventArgs e)
+        {
+            ChangeSoLuong();
+        }
+
+        private void ChangeSoLuong()
+        {
+            solong = lstThongTin.Items.Count;
+            txtSoLuong.Text = solong.ToString();
+        }
+        private void guna2BtnNhap_Click(object sender, EventArgs e)
+        {
+
+            if (TxtTen != null && lstThongTin != null)
+            {
+                if (!String.IsNullOrEmpty(TxtTen.Text))
+                {
+                    lstThongTin.Items.Add(TxtTen.Text);
+                    ChangeSoLuong();
+                    //  txt_hienthi.Text = lstBox_hienthi.Items.Count.ToString();
+                    TxtTen.Clear();
+                    TxtTen.Focus();
+                }
+                else
+                    MessageBox.Show("Vui lòng điền đầy đủ thông tin !!!");
+            }
+        }
+        private void btnXTTODau_Click(object sender, EventArgs e)
+        {
+            // int a = Convert.ToInt32(TxtTen.Text);
+            if (lstThongTin.Items.Count > 0)
+            {
+                lstThongTin.Items.RemoveAt(0);
+                ChangeSoLuong();
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống !", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void btnXTTOCuoi_Click(object sender, EventArgs e)
+        {
+            if (lstThongTin.Items.Count > 0)
+            {
+                lstThongTin.Items.Remove(lstThongTin.Items[lstThongTin.Items.Count - 1]);
+                ChangeSoLuong();
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống !", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void btnXTTDangChon_Click(object sender, EventArgs e)
+        {
+            if (lstThongTin.Items.Count > 0)
+            {
+                /*for (int i = lstThongTin.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    lstThongTin.Items.RemoveAt(lstThongTin.SelectedIndices[i]);
+                }*/ // c1;
+                foreach (var item in lstThongTin.SelectedItems.Cast<object>().ToList())
+                {
+                    lstThongTin.Items.Remove(item);
+                }
+                ChangeSoLuong();
+
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống !", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Bạn muốn xóa tất cả thông tin trong danh sách ?", "Cảnh báo!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlr == DialogResult.Yes)
+            {
+                if (lstThongTin.Items.Count > 0)
+                {
+                    lstThongTin.Items.Clear();
+                    ChangeSoLuong();
+                }
+                else
+                {
+                    MessageBox.Show("Danh sách trống !", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Demo2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                guna2BtnNhap_Click(sender , e);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,17 @@
-﻿using System;
-
-Random rand = new Random();
-int so = 0;
-
-Console.WriteLine("Truong hop  1 : ");
-for( int i = 0; i < 10; i++)
+namespace Do_Hoa_Winform
 {
-    so = rand.Next(); // Ham cho ra gia tri nguyen lon hơn bằng 0;
-    Console.WriteLine("So ngau nhien la : {0} ", so);
-}
-
-Console.WriteLine("Truong hop  2 : ");
-for ( int i = 0; i < 10; i++)
-{
-    so = rand.Next(5); // ham cho ra gia tri <= 5-1;
-    Console.WriteLine("So ngau nhien la : {0} ", so);
-}
-
-Console.WriteLine("Truong hop  3 : ");
-for (int i = 0; i < 10; i++)
-{
-    so = rand.Next(-10 , 5); // ham cho ra gia tri từ -10->5-1;
-    Console.WriteLine("So ngau nhien la : {0}", so);
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Demo2());
+        }
+    }
 }
