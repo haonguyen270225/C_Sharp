151d9ad1df717e222cdad271ebdd33e8a9a6f2b5
HaoNhat <haonguyennhat270225@gmail.com>
2025-12-01 00:27:45 +0700

    Learn Program_Structure_Of_C#_Program Chuong1/Bai3 !

---
+++ C_Sharp/CSharp_Basic/Program_Structure_Of_C#_Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+Console.WriteLine("hhhhhhhhhhh!");
+namespace YourNamespace
+{
+    class YourClass
+    {
+
+    }
+
+    struct YourStruct
+    {
+
+    }
+
+    delegate void YourDelegate();
+    
+    enum YourEnum
+    {
+
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourNamespace
+        {
+
+        }
+    }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello world !");
+    //    }
+    //}
+}
