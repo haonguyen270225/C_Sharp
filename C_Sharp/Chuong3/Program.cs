3d9f6e9473b82b0d79530c0df8de5b55df88ac11
HaoNhat <haonguyennhat270225@gmail.com>
2025-12-02 18:47:33 +0700

    Chuong3/Bai1 !

---
+++ C_Sharp/Chuong3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Chuong3
+{
+    class Bai1
+    {
+        private string[] name = ["Spencer", "Sally", "Doug"];
+        public string GetName(int ID)
+        {
+            if(ID < name.Length)
+            {
+                return name[ID];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bai1 bai1 = new Bai1();
+            Console.WriteLine($"{bai1.GetName(0)}");
+            Console.WriteLine($"{bai1.GetName(5)}");
+        }
+    }
+}
