7771a85d4c538989cad36e75e0686e23eb54f675
HaoNhat <haonguyennhat270225@gmail.com>
2025-12-15 01:21:46 +0700

    Learn Generic_Classes_And_Methods !

---
+++ C_Sharp/CSharp_Basic/Generic_Class_And_Methods/GenericList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Basic.Generic_Class_And_Methods
+{
+
+    public class ExampleClass { };
+    internal class GenericList<T>
+    {
+        public void Add(T item) { }
+    }
+}
--- C_Sharp/CSharp_Basic/Program.cs
+++ C_Sharp/CSharp_Basic/Program.cs
@@ -1,22 +1,32 @@
 ﻿using Variable;
 using CSharp_Basic.OOP;
 using System.Text;
+using LearnTryCatch;
+using System;
+using CSharp_Basic.Generic_Class_And_Methods;
 namespace CSharp_Basic
 {
     internal class Program
     {
+        static void  Method_Generic()
+        {
+            GenericList<int> list = new();
+            list.Add(1);
+
+            GenericList<string> list2 = new();
+            list2.Add("");
+
+            GenericList<ExampleClass> list3 = new();
+            list3.Add(new ExampleClass());
+        }
+       
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.UTF8;
 
-            //DanhSachSinhVien listSinhVien = new DanhSachSinhVien(3);
-            ////listSinhVien = new DanhSachSinhVien(2);
-            //listSinhVien.SetDanhSachSinhVien();
-            //listSinhVien.GetDanhSachSinhVien();
-
-            Format_WriteLines.LearnWriteLine();
-            Console.ReadKey();
+            Method_Generic();
+            
         }
     }
 }
